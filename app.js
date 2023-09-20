dfed12acf15149c995fb9f8d92250785e780da0f
Damilp7 <ayoolabamgbelu@gmail.com>
2023-09-20 12:11:49 +0100

    fixes

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-function weightCheck(name, weight) {
-  name = prompt("Input your name:");
-  weight = Number(prompt("Input your weight in Kg:"));
-  return `${name}'s Weight in Pounds: ${weight * 2.20462}lbs`
-  }
+// function weightCheck(name, weight) {
+//   name = prompt("Input your name:");
+//   weight = Number(prompt("Input your weight in Kg:"));
+//   return `${name}'s Weight in Pounds: ${weight * 2.20462}lbs`
+//   }
   
-  alert(weightCheck());
+//   alert(weightCheck());
+
+let name = prompt("Input your name:");
+let weight = Number(prompt("Input your weight in Kg:"));
+
+function weightCheck() {
+ return `${name}'s weight in Pounds: ${weight * 2.20462}lbs`
+}
+alert(weightCheck());
